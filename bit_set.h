2d8a599b04a503bee64e2885a1a82655c663fdc5
kalen <jkalend@gmail.com>
2022-03-11 15:08:25 +0100

    Create bit_set.h

---
+++ bit_set.h
@@ -0,0 +1,10 @@
+//bit_set.h
+//author: Jan Kalenda (xkalen07), VUT FIT
+
+#include <limits.h>
+#include <stdlib.h>
+//#include "error.h"
+#include <stdio.h>
+#include <assert.h>
+
+#ifndef BIT_ARRAY_H
